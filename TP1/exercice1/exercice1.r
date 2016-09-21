fa04e5ed2f51544db580ef9132bfd370b65f7aa0
Valentin Paul <valentin.paul@etu.utc.fr>
2016-09-21 17:44:08 +0200

    end of exercice 1 normally ... not sure because Marie & Antoine haven't exactly the same answer ...

--- TP1/exercice1/exercice1.r
+++ TP1/exercice1/exercice1.r
@@ -1,8 +1,17 @@
 
 prostate.data = read.table("prostate.data.txt")
-plot(prostate.data,col='blue',pch=20)
 install.packages('FNN')
+install.packages("automap")
+install.packages("spacetime")
+install.packages("sp")
+install.packages("zoo")
+install.packages("hydroGOF")
 
+# ===================================
+# REGRESSION PAR LA METHODE DES KPPVS
+# ===================================
+
+plot(prostate.data,col='blue',pch=20)
 
 # Valeur de train sans etiquette
 train = subset(prostate.data,train == T, select=c(lcavol,lweight,lbph,age))
@@ -10,14 +19,26 @@ train = subset(prostate.data,train == T, select=c(lcavol,lweight,lbph,age))
 test = subset(prostate.data,train == F, select=c(lcavol,lweight,lbph,age))
 
 # Etiquette de valeur de train
-lpsa.train = subset(prostate.data,train == T, select=c(lpsa)
+lpsa.train = subset(prostate.data,train == T, select=c(lpsa))
 # Etiquette de valeur de test
 lpsa.test = subset(prostate.data,train == F, select=c(lpsa))
 
-# Lance Knn et recuperation des étiquettes selon l'algo
-knn = knn.reg(train, test, lpsa.train, k = 5)
-test.prediction = knn$pred
-
 # Plot le resultat => Comparaison entre les etiquettes de prediction et les etiquettes de base de test
-comparison <- data.frame(lpsa.test, test.prediction)
-plot(comparison)
+#comparison <- data.frame(lpsa.test, test.prediction)
+#plot(comparison)
+kValues = 3:30
+kMSE = rep(0,length(kValues))
+
+for(kValue in kValues) {
+    # Lance Knn et recuperation des étiquettes selon l'algo
+    knn = knn.reg(train, test, lpsa.train, k = kValue)
+    test.prediction = knn$pred
+
+    # Transformation in matrix for comparition values
+    test.prediction = matrix(test.prediction)
+    kMSE[kValue-2] = mse(test.prediction,lpsa.test)
+}
+
+resultKGraph = data.frame(kValues,kErrors)
+plot(resultKGraph,type='b')
+
