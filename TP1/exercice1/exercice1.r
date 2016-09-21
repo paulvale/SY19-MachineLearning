c45215fa0ecbe9fd9db8be5d72d1fd75568ea85c
Valentin Paul <valentin.paul@etu.utc.fr>
2016-09-21 18:16:01 +0200

    amelioration of visualisation of data !

--- TP1/exercice1/exercice1.r
+++ TP1/exercice1/exercice1.r
@@ -1,11 +1,22 @@
 
+# LOAD DATA
 prostate.data = read.table("prostate.data.txt")
-install.packages('FNN')
+
+# INSTALLATION PACKAGES
+install.packages("FNN")
 install.packages("automap")
 install.packages("spacetime")
 install.packages("sp")
 install.packages("zoo")
 install.packages("hydroGOF")
+
+# LOAD PACKAGES
+library("FNN", character.only = TRUE)
+library("automap", character.only = TRUE)
+library("spacetime", character.only = TRUE)
+library("sp", character.only = TRUE)
+library("zoo", character.only = TRUE)
+library("hydroGOF", character.only = TRUE)
 
 # ===================================
 # REGRESSION PAR LA METHODE DES KPPVS
@@ -27,18 +38,34 @@ lpsa.test = subset(prostate.data,train == F, select=c(lpsa))
 #comparison <- data.frame(lpsa.test, test.prediction)
 #plot(comparison)
 kValues = 3:30
-kMSE = rep(0,length(kValues))
+kMSE.train = rep(0,length(kValues))
+kMSE.test = rep(0,length(kValues))
+
 
 for(kValue in kValues) {
     # Lance Knn et recuperation des étiquettes selon l'algo
-    knn = knn.reg(train, test, lpsa.train, k = kValue)
-    test.prediction = knn$pred
+    knn.train = knn.reg(train, train, lpsa.train, k = kValue)
+    train.prediction = knn.train$pred
+
+    # Transformation in matrix for comparition values
+    train.prediction = matrix(train.prediction)
+    kMSE.train[kValue-2] = mse(train.prediction,lpsa.train)
+
+    # =====================================================
+
+    # Lance Knn et recuperation des étiquettes selon l'algo
+    knn.test = knn.reg(train, test, lpsa.train, k = kValue)
+    test.prediction = knn.test$pred
 
     # Transformation in matrix for comparition values
     test.prediction = matrix(test.prediction)
-    kMSE[kValue-2] = mse(test.prediction,lpsa.test)
+    kMSE.test[kValue-2] = mse(test.prediction,lpsa.test)
+
 }
 
-resultKGraph = data.frame(kValues,kErrors)
-plot(resultKGraph,type='b')
-
+resultKGraph.train = data.frame(kValues,kMSE.train)
+resultKGraph.test = data.frame(kValues,kMSE.test)
+plot(resultKGraph.train,type='b', ylim=range(c(kMSE.train, kMSE.test)))
+# second plot  EDIT: needs to have same ylim
+par(new = TRUE)
+plot(resultKGraph.test,type='b', col='red', ylim=range(c(kMSE.train, kMSE.test)), axes = FALSE, xlab = "", ylab = "")
