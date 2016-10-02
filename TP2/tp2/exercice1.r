43719cfa6c478b79866feff51d868271298b3435
Valentin Paul <valentin.paul@etu.utc.fr>
2016-10-02 11:02:07 +0200

    exercice 1 in advance : Q1 - Q5 done Q6 - Q8 still need to be done

--- TP2/tp2/exercice1.r
+++ TP2/tp2/exercice1.r
@@ -5,17 +5,95 @@ prostate.data = read.table("data/prostate.data.txt")
 #Linear regression method
 reg.smsa <- lm(prostate.data$lpsa~prostate.data$lcavol+prostate.data$lweight+prostate.data$age+prostate.data$lbph+prostate.data$svi+prostate.data$lcp+prostate.data$gleason+prostate.data$pgg45)
 
-#Coefficients:
-#          (Intercept)   prostate.data$lcavol  prostate.data$lweight  
-#             0.181561               0.564341               0.622020  
-#   prostate.data$age     prostate.data$lbph      prostate.data$svi  
-#            -0.021248               0.096713               0.761673  
-#    prostate.data$lcp  prostate.data$gleason    prostate.data$pgg45  
-#            -0.106051               0.049228               0.004458 
+# === Q1 ===
+# Les coefficients significativement non nuls sont lcavol, lweigth,svi
+# En notation anglo saxonne, "intercept" correspond a l'ordonnee a l'origine
+# correspond donc ici au "b" de notre equation f(x) = ax + b
 
-#Les coefficients significativment non nuls sont lcavol, lweight, svi
-#		En notation anglo-saxonne, « Intercept » correspond ici à l’ordonnée 
-#		à l’origine le « b » de notre droite et le « x » est la pente de la droite 
-#		ce qui correspond au « b » dans notre notation
+# avoir avoir realise un summary sur notre regression, on peut alors voir
+# qu'il y a en effet 3 facteurs determinants sur la prediction de nos valeurs lpsa
+# la plus importante est lcavol, mais il y a aussi lweight and svi.
+print(summary(reg.smsa))
+# les residus (differences entre valeurs originales et valeurs estimees)
+# p-val correspond au test de Fisher
+# si cette valeur est superieure a 0.05 alors notre regression n'est pas significative.
+# Ici, on a une p-value = 2.2e-16 qui est donc bien inferieure a 0.05
+# => Notre regression est globalement significative
+plot(prostate.data$lcavol,prostate.data$lpsa)
+readline(prompt="Press [enter] to continue")
 
-#f(x)= 0.181561  + 0.564341x1 + 0.622020x2 -0.021248x3 + 0.096713x4 +0.761673x5 -0.106051x6 +0.049228x7 + 0.004458x8
+# === Q2 ===
+print(confint(reg.smsa,level=0.95))
+
+# === Q3 ===
+plot(prostate.data$lpsa,prostate.data$lpsa,col="black")     
+abline(a=0,b=1)     
+points(prostate.data$lpsa,reg.smsa$fitted.values,pch=19,col="red",cex=0.7)
+readline(prompt="Press [enter] to continue")
+
+# === Q4 ===
+# Pour les residus brute :
+print("les residus bruts :")
+plot(prostate.data$lpsa,reg.smsa$residuals,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,reg.smsa$residuals,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,reg.smsa$residuals,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+# A la vue de ces 3 graphiques, on a l'impression que l'on a une evolution croissante des residus 
+# en fonction de notre valeur de lpsa, cependant pour les 2 autres variables lweight et lcavol, 
+# on n'arrive pas a distinguer une relation entre les residus et ces variables ...
+
+# Pour les residus centre reduit:
+residualsStd <- rstandard(reg.smsa)
+print("les residus standardises :")
+plot(prostate.data$lpsa,residualsStd,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,residualsStd,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,residualsStd,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+# Une fois standardise, la relation entre lpsa et les residus est d'autant plus marque,
+# en effet, plus la valeur de lpsa est grande plus les residus sont importants et inversement.
+
+
+# Pour les residus studentise:
+residualsStudent <- rstudent(reg.smsa)
+print("les residus studentises :")
+plot(prostate.data$lpsa,residualsStudent,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,residualsStudent,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,residualsStudent,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+# Meme observation que precedement
+
+# === Q5 ===
+# Le QQ plot est un graphe qui nous permet de verifier graphiquement la normalite de notre regression
+# celui est notamment accessible directement via nos data de reg.smsa
+
+# Comme on peut l'observer, nous avons belle et bien une droite et pouvons donc 
+# conclure via ce graphique que nos residus suivent bien une loi normale
+plot(reg.smsa, which = 2)
+
+
+
+
