47aed621c7c517d849cfd99420429fd2aa854dcf
mleriche <marceau.leriche@gmail.com>
2016-10-05 09:43:02 +0200

    Question 6 et 7 du TP2 - mise en place du tp3

--- TP2/tp2/exercice1.r
+++ TP2/tp2/exercice1.r
@@ -95,12 +95,82 @@ readline(prompt="Press [enter] to continue")
 plot(reg.smsa, which = 2)
 
 # === Q6 ===
-# Pour voir la stabilité du modèle (influence de chaque observation sur les coefficients estimés Bi) il faut utiliser la distance de Cook
+#   Pour voir la stabilité du modèle (influence de chaque observation sur les coefficients estimés Bi) il faut utiliser la distance de Cook. Cook mesure l'effet de la suppression de l'observation i sur la prédiction de toutes les valeurs
 d1 <- cooks.distance(reg.smsa)
 plot(d1)
-#   On peut observer sur le graphe que les points ne sont pas abérants (bien inférieur à 1), les valeurs ne sont donc pas trop atypique.
+#   On peut observer sur le graphe que les points ne sont pas abérants (bien inférieur à 1), les valeurs ne sont donc pas trop atypiques. On peut aussi utiliser : plot(reg.smsa,which=4)
 
 
+# === Q7 ===
+#   j'ai repris le script avec seulement 2 predicteurs, les plus significatifs : lcavol et lweight
+reg.smsa2 <- lm(prostate.data$lpsa~prostate.data$lcavol+prostate.data$lweight)
+print(summary(reg.smsa2))
+print(confint(reg.smsa2,level=0.95))
+#   On peut voir que les intervalles de confiance réduisent (0.35 avant et 0.27 maintenant pour lcavol, même phénomène pour lweight) les valeurs sont donc plus concentrés autour de la moyenne
+
+plot(prostate.data$lpsa,prostate.data$lpsa,col="black")
+abline(a=0,b=1)
+points(prostate.data$lpsa,reg.smsa2$fitted.values,pch=19,col="red",cex=0.7)
+#   répartition équivalente à l'autre sous ensemble de prédicteur
+
+# Pour les residus bruts :
+print("les residus bruts :")
+plot(prostate.data$lpsa,reg.smsa2$residuals,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,reg.smsa2$residuals,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,reg.smsa2$residuals,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+# Pour les residus centre reduit:
+residualsStd2 <- rstandard(reg.smsa2)
+print("les residus standardises :")
+plot(prostate.data$lpsa,residualsStd2,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,residualsStd2,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,residualsStd2,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+
+# Pour les residus studentise:
+residualsStudent2 <- rstudent(reg.smsa2)
+print("les residus studentises :")
+plot(prostate.data$lpsa,residualsStudent2,col="red")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lcavol,residualsStudent2,col="blue")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+plot(prostate.data$lweight,residualsStudent2,col="black")
+abline(a=0,b=0)
+readline(prompt="Press [enter] to continue")
+
+# QQPLOT plus fitté à la courbe linéaire
+plot(reg.smsa2, which = 2)
+readline(prompt="Press [enter] to continue")
+
+# Distance de COOK - graphe plus ou moins équivalent, pas d'observations trop atypiques
+plot(reg.smsa2,which=4)
+readline(prompt="Press [enter] to continue")
+plot(reg.smsa,which=4)
+
+# Pour résumer, on peut voir que les résultats sont presque équivalent que lorsqu'on avait tous les predicteurs. On peut expliquer cela par le fait qu'on ait pris les predicteurs les plus significatifs sur cette regression lineaire.
+
+#   === Q8 ===
+#   Effectuer quelques transformations non linéaires de certains predicteurs
 
 
 
