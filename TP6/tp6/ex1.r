dc93d5ba0cb987bb8dbb68037e3c2f2242b95ae5
mleriche <marceau.leriche@gmail.com>
2016-11-30 11:45:41 +0100

    generate ok

--- TP6/tp6/ex1.r
+++ TP6/tp6/ex1.r
@@ -2,9 +2,12 @@ generate <- function(mu,sigma,pii,a){
 
 	normale <- rnorm(100*pii,mu,sigma)
 	uniforme <- runif(100*(1-pii),-a,a)
-	x <- sample(c(normale,uniforme),size=100,replace=FALSE)
+	x <- sample(c(normale,uniforme),size=100,replace=TRUE)
 	return(x)
 }
+
+
+boxplot(generate(0,1,0.8,5))
 
 # Nous avons 2 loi dans ce mélange de modeles  : 
 #	-loi normale (mu,sigma)
