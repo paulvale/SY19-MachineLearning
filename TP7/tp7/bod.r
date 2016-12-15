972e0fe958f7092cb7ef6deaca4cce7056eb4c7a
Valentin Paul <valentin.paul@etu.utc.fr>
2016-12-15 11:06:35 +0100

    useless commit

--- TP7/tp7/bod.r
+++ TP7/tp7/bod.r
@@ -1,5 +1,16 @@
+# LOAD PACKAGES 
+# =============
+load("~/Documents/UTC/A16/SY19/TPs_Desktop/TP/TP7/tp7/data/data_expressions.RData")
+X.dim <- dim(X)
+
+for( i in 1:X.dim[1]){
+  I<-matrix(X[i,],60,70)
+  I1 <- apply(I, 1, rev)
+  image(t(I1),col=gray(0:255 / 255))
+}
+
+
 acp.X <- prcomp(X)
 plot(acp.X)
-print(acp.X$sdev)
-print("===")
-print(100 * acp.X$sdev^2 / sum(acp.X$sdev^2))
+print(cumsum(100 * acp.X$sdev^2 / sum(acp.X$sdev^2)))
+
