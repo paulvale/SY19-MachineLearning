ce4f065eac607c5d08d25bec1f3bb60a684761e7
Valentin Paul <valentin.paul@etu.utc.fr>
2016-12-18 11:18:00 +0100

    random forest done

--- TP7/tp7/bod.r
+++ TP7/tp7/bod.r
@@ -5,6 +5,8 @@ load("~/Documents/UTC/A16/SY19/TPs_Desktop/TP/TP7/tp7/data/data_expressions.RDat
 library(MASS)
 library(leaps)
 library(corrplot)
+library(randomForest)
+library(ROCR)
 
 X.dim <- dim(X)
 
@@ -133,8 +135,7 @@ boxplot(X.lda.transform[,1]~y.app, main="LDA Data",
         xlab="LD1 value", ylab="faces expression value")
 
 # 2) ACP
-pairs(~.,data=X.acp.transform1[,1:5], col=y.app, main="ACP Data 1 (seuil de 75)")
-pairs(~.,data=X.acp.transform2[,1:5], col=y.app, main="ACP Data 2 (seuil de 85)")
+pairs(~.,data=X.acp.transform1[,1:5], col=y.app, main="ACP Data")
 par(mfrow = c(1, 1))
 boxplot(X.acp.transform1[,1]~y.app, main="ACP Data", 
         xlab="PC1 value", ylab="faces expression value")
@@ -147,7 +148,213 @@ corrplot(X.forward.cor, type="lower", tl.cex = 0.6)
 boxplot(X.forward[,1]~y.app, main="Forward Selection Data", 
         xlab="Forward1 value", ylab="faces expression value")
 
+## ===
+## Random Forest
+## ===
+# calcul des taux d'erreur
+vectorTree <- seq(100,1500,100)
+
+# ===
+# acp1
+# ===
+# err
+rF.acp1.tauxErreur.test <- rep(0,length(vectorTree))
+rF.acp1.tauxErreur.app <- rep(0,length(vectorTree))
+
+# precision
+rF.acp1.precision.test <- rep(0,length(vectorTree))
+rF.acp1.precision.app <- rep(0,length(vectorTree))
+
+# rappel
+rF.acp1.rappel.test <- rep(0,length(vectorTree))
+rF.acp1.rappel.app <- rep(0,length(vectorTree))
+
+# ===
+# acp2
+# ===
+# err
+rF.acp2.tauxErreur.test <- rep(0,length(vectorTree))
+rF.acp2.tauxErreur.app <- rep(0,length(vectorTree))
+
+# precision
+rF.acp2.precision.test <- rep(0,length(vectorTree))
+rF.acp2.precision.app <- rep(0,length(vectorTree))
+
+# rappel
+rF.acp2.rappel.test <- rep(0,length(vectorTree))
+rF.acp2.rappel.app <- rep(0,length(vectorTree))
 
 
+# ===
+# forward
+# ===
+# err
+rF.forward.tauxErreur.test <- rep(0,length(vectorTree))
+rF.forward.tauxErreur.app <- rep(0,length(vectorTree))
+
+# precision
+rF.forward.precision.test <- rep(0,length(vectorTree))
+rF.forward.precision.app <- rep(0,length(vectorTree))
+
+# rappel
+rF.forward.rappel.test <- rep(0,length(vectorTree))
+rF.forward.rappel.app <- rep(0,length(vectorTree))
+
+# ===
+# lda
+# ===
+# err
+rF.lda.tauxErreur.test <- rep(0,length(vectorTree))
+rF.lda.tauxErreur.app <- rep(0,length(vectorTree))
+
+# preicision
+rF.lda.precision.test <- rep(0,length(vectorTree))
+rF.lda.precision.app <- rep(0,length(vectorTree))
+
+# rappel
+rF.lda.rappel.test <- rep(0,length(vectorTree))
+rF.lda.rappel.app <- rep(0,length(vectorTree))
+
+for(i in vectorTree) {
+  print(i)
+  rF.acp1 <- randomForest(factor(y.app)~., data=as.data.frame(X.acp.transform1), xtest=as.data.frame(X.acp.test.transform1), ytest=factor(y.test), ntree=i)
+  #print("ACP1 RandomForest :")
+  #print(rF.acp1$test$confusion)
+  
+  rF.acp2 <- randomForest(factor(y.app)~., data=as.data.frame(X.acp.transform2), xtest=as.data.frame(X.acp.test.transform2), ytest=factor(y.test), ntree=i)
+  #print("ACP1 RandomForest :")
+  #print(rF.acp2$test$confusion)
+  
+  rF.lda <- randomForest(factor(y.app)~., data=as.data.frame(X.lda.transform), xtest=as.data.frame(X.lda.transform.test), ytest=factor(y.test), ntree=i)
+  #print("LDA RandomForest :")
+  #print(rF.lda$test$confusion)
+  
+  rF.forward <- randomForest(factor(y.app)~., data=as.data.frame(X.forward), xtest=as.data.frame(X.forward.test), ytest=factor(y.test), ntree=i)
+  #print("Forward Stepwise RandomForest :")
+  #print(rF.forward$test$confusion)
+  
+  # Calcul des taux d'erreurs
+  rF.acp1.tauxErreur.test[i/100] <- 1 - (sum(diag(rF.acp1$test$confusion)/X.test.dim[1]))
+  rF.acp2.tauxErreur.test[i/100] <- 1 - (sum(diag(rF.acp2$test$confusion)/X.test.dim[1]))
+  rF.lda.tauxErreur.test[i/100] <- 1 - (sum(diag(rF.lda$test$confusion)/X.test.dim[1]))
+  rF.forward.tauxErreur.test[i/100] <- 1 - (sum(diag(rF.forward$test$confusion)/X.test.dim[1]))
+  
+  rF.acp1.tauxErreur.app[i/100] <- 1 - (sum(diag(rF.acp1$confusion)/X.app.dim[1]))
+  rF.acp2.tauxErreur.app[i/100] <- 1 - (sum(diag(rF.acp2$confusion)/X.app.dim[1]))
+  rF.lda.tauxErreur.app[i/100] <- 1 - (sum(diag(rF.lda$confusion)/X.app.dim[1]))
+  rF.forward.tauxErreur.app[i/100] <- 1 - (sum(diag(rF.forward$confusion)/X.app.dim[1]))
+  
+  for( j in 1:6){
+    # Calcul de la precision
+    rF.acp1.precision.test[i/100] <- rF.acp1.precision.test[i/100] + if(sum(rF.acp1$test$confusion[,j]) == 0) 0 else (rF.acp1$test$confusion[j,j]/sum(rF.acp1$test$confusion[,j]))
+    rF.acp2.precision.test[i/100] <- rF.acp2.precision.test[i/100] + if(sum(rF.acp2$test$confusion[,j]) == 0) 0 else (rF.acp2$test$confusion[j,j]/sum(rF.acp2$test$confusion[,j]))
+    rF.lda.precision.test[i/100] <- rF.lda.precision.test[i/100] + if(sum(rF.lda$test$confusion[,j]) == 0) 0 else (rF.lda$test$confusion[j,j]/sum(rF.lda$test$confusion[,j]))
+    rF.forward.precision.test[i/100] <- rF.forward.precision.test[i/100] + if(sum(rF.forward$test$confusion[,j]) == 0) 0 else (rF.forward$test$confusion[j,j]/sum(rF.forward$test$confusion[,j]))
+    
+    rF.acp1.precision.app[i/100] <- rF.acp1.precision.app[i/100] + if(sum(rF.acp1$confusion[,j]) == 0) 0 else (rF.acp1$confusion[j,j]/sum(rF.acp1$confusion[,j]))
+    rF.acp2.precision.app[i/100] <- rF.acp2.precision.app[i/100] + if(sum(rF.acp2$confusion[,j]) == 0) 0 else (rF.acp2$confusion[j,j]/sum(rF.acp2$confusion[,j]))
+    rF.lda.precision.app[i/100] <- rF.lda.precision.app[i/100] + if(sum(rF.lda$confusion[,j]) == 0) 0 else (rF.lda$confusion[j,j]/sum(rF.lda$confusion[,j]))
+    rF.forward.precision.app[i/100] <- rF.forward.precision.app[i/100] + if(sum(rF.forward$confusion[,j]) == 0) 0 else (rF.forward$confusion[j,j]/sum(rF.forward$confusion[,j]))
+    
+    # Calcul du rappel
+  }
+  rF.acp1.precision.test[i/100] <- (rF.acp1.precision.test[i/100]/6)*100
+  rF.acp2.precision.test[i/100] <- (rF.acp2.precision.test[i/100]/6)*100
+  rF.lda.precision.test[i/100] <- (rF.lda.precision.test[i/100]/6)*100
+  rF.forward.precision.test[i/100] <- (rF.forward.precision.test[i/100]/6)*100
+  
+  rF.acp1.precision.app[i/100] <- (rF.acp1.precision.app[i/100]/6)*100
+  rF.acp2.precision.app[i/100] <- (rF.acp2.precision.app[i/100]/6)*100
+  rF.lda.precision.app[i/100] <- (rF.lda.precision.app[i/100]/6)*100
+  rF.forward.precision.app[i/100] <- (rF.forward.precision.app[i/100]/6)*100
+  
+}
+# ===
+# Graphes
+# ===
+
+# Taux d'erreur
+# acp1
+min <- min(c(rF.acp1.tauxErreur.app, rF.acp1.tauxErreur.test))
+max <- max(c(rF.acp1.tauxErreur.app, rF.acp1.tauxErreur.test))
+plot(vectorTree,rF.acp1.tauxErreur.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.acp1.tauxErreur.test,col="green", type="l", ylim=c(min,max), ylab="taux d'erreur", main="Taux erreur ACP 1")
+
+# acp2
+min <- min(c(rF.acp2.tauxErreur.app, rF.acp2.tauxErreur.test))
+max <- max(c(rF.acp2.tauxErreur.app, rF.acp2.tauxErreur.test))
+plot(vectorTree,rF.acp2.tauxErreur.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.acp2.tauxErreur.test,col="green", type="l", ylim=c(min,max), ylab="taux d'erreur", main="Taux erreur ACP 2")
+
+# lda
+min <- min(c(rF.lda.tauxErreur.app, rF.lda.tauxErreur.test))
+max <- max(c(rF.lda.tauxErreur.app, rF.lda.tauxErreur.test))
+plot(vectorTree,rF.lda.tauxErreur.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.lda.tauxErreur.test,col="green", type="l", ylim=c(min,max), ylab="taux d'erreur", main="Taux erreur LDA")
+
+# forward
+min <- min(c(rF.forward.tauxErreur.app, rF.forward.tauxErreur.test))
+max <- max(c(rF.forward.tauxErreur.app, rF.forward.tauxErreur.test))
+plot(vectorTree,rF.forward.tauxErreur.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.forward.tauxErreur.test,col="green", type="l", ylim=c(min,max), ylab="taux d'erreur", main="Taux erreur Forward")
 
 
+# Precision
+# acp1
+min <- min(c(rF.acp1.precision.app, rF.acp1.precision.test))
+max <- max(c(rF.acp1.precision.app, rF.acp1.precision.test))
+plot(vectorTree,rF.acp1.precision.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.acp1.precision.test,col="green", type="l", ylim=c(min,max), ylab="%", main="Precision ACP 1")
+
+# acp2
+min <- min(c(rF.acp2.precision.app, rF.acp2.precision.test))
+max <- max(c(rF.acp2.precision.app, rF.acp2.precision.test))
+plot(vectorTree,rF.acp2.precision.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.acp2.precision.test,col="green", type="l", ylim=c(min,max), ylab="%", main="Precision ACP 2")
+
+# lda
+min <- min(c(rF.lda.precision.app, rF.lda.precision.test))
+max <- max(c(rF.lda.precision.app, rF.lda.precision.test))
+plot(vectorTree,rF.lda.precision.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.lda.precision.test,col="green", type="l", ylim=c(min,max), ylab="%", main="Precision LDA")
+
+# forward
+min <- min(c(rF.forward.precision.app, rF.forward.precision.test))
+max <- max(c(rF.forward.precision.app, rF.forward.precision.test))
+plot(vectorTree,rF.forward.precision.app,type="l",col="red", ylim=c(min,max), ylab="")
+par(new=TRUE)
+plot(vectorTree,rF.forward.precision.test,col="green", type="l", ylim=c(min,max), ylab="%", main="Precision Forward")
+
+# Observations faites sur les graphes :
+# on se rend comtpe ici que forward est le seule algos qui nous donnent une erreur d'apprentissage 
+# superieur a notre erreur de test
+
+# Cependant dans tous les cas il est tres eleve ( min 65% taux d'erreur)
+# => LDA
+# ntree = 200 
+# taux erreur app : 5%
+#             test : 28%
+# est ce que on ne serait pas en overfitting ?
+
+# => ACP1
+# ntree = 200 
+# taux erreur app : 28%
+#             test : 50%
+
+# => ACP2
+# ntree = 100 
+# taux erreur app : 31%
+#             test : 45%
+
+# ===> Notre meilleur modele c'est le LDA pour le moment avec les random forests
+
+## ===
+## Random Forest
+## ===
