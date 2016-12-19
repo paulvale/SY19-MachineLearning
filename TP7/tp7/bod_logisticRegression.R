aa9a32cc2a46b8a4bf2736c5fafd10ca6a679e1d
Valentin Paul <valentin.paul@etu.utc.fr>
2016-12-19 08:47:54 +0100

    add full acp to the log regression file

--- TP7/tp7/bod_logisticRegression.R
+++ TP7/tp7/bod_logisticRegression.R
@@ -68,6 +68,7 @@ X.lda.transform <- X.app%*%X.lda$scaling
 X.acp <- prcomp(X.app)
 acp.sum <- cumsum(100 * X.acp$sdev^2 / sum(X.acp$sdev^2))
 
+X.acp.full <- X.acp$x
 X.acp.transform1 <- X.acp$x[,which(acp.sum<75)]
 X.acp.transform2 <- X.acp$x[,which(acp.sum<85)]
 
@@ -89,6 +90,7 @@ X.lda.transform.test <- X.test%*%X.lda$scaling
 # 2) ACP
 X.acp.test <- X.test %*% X.acp$rotation
 
+X.acp.test.full <- X.acp.test
 X.acp.test.transform1 <- X.acp.test[,which(acp.sum<75)]
 X.acp.test.transform2 <- X.acp.test[,which(acp.sum<85)]
 
@@ -96,12 +98,12 @@ X.acp.test.transform2 <- X.acp.test[,which(acp.sum<85)]
 X.forward.test <- X.test[,which(reg.fit$vorder < ind)]
 
 
-
 # ===
 # Logistic Regression
 # ===
 K <- 5 # Nombre de sections dans notre ensemble d'apprentissage
 folds <- sample(1:K,X.app.dim[1] ,replace=TRUE)
+logReg.acpF.error <- 0
 logReg.acp1.error <- 0
 logReg.acp2.error <- 0
 logReg.lda.error <- 0
@@ -109,11 +111,15 @@ logReg.forward.error <- 0
 
 for(i in 1:K){
   numberTest <- dim(X.acp.transform1[folds==i,])[1]
+  logReg.acpF.res<-c(rep(0,numberTest))
   logReg.acp1.res<-c(rep(0,numberTest))
   logReg.acp2.res<-c(rep(0,numberTest))
   logReg.lda.res<-c(rep(0,numberTest))
   logReg.forward.res<-c(rep(0,numberTest))
-  
+
+  logReg.acpF <- glmnet(X.acp.full[folds!=i,] ,y=y.app[folds!=i],family="multinomial")
+  logReg.acpF.pred <- predict(logReg.acpF,newx=X.acp.full[folds==i,],type="response",s=logReg.acpF$lambda.min)
+    
   logReg.acp1 <- glmnet(X.acp.transform1[folds!=i,] ,y=y.app[folds!=i],family="multinomial")
   logReg.acp1.pred <- predict(logReg.acp1,newx=X.acp.transform1[folds==i,],type="response",s=logReg.acp1$lambda.min)
   
@@ -127,11 +133,15 @@ for(i in 1:K){
   logReg.forward.pred <- predict(logReg.forward,newx=X.forward[folds==i,],type="response",s=logReg.forward$lambda.min)
   
   for (h in 1:numberTest) {
+    logReg.acpF.res[h] <-which.max(logReg.acpF.pred[h,1:6,dim(logReg.acpF.pred)[3]])
     logReg.acp1.res[h] <-which.max(logReg.acp1.pred[h,1:6,dim(logReg.acp1.pred)[3]])
     logReg.acp2.res[h] <-which.max(logReg.acp2.pred[h,1:6,dim(logReg.acp2.pred)[3]])
     logReg.lda.res[h] <-which.max(logReg.lda.pred[h,1:6,dim(logReg.lda.pred)[3]])
     logReg.forward.res[h] <-which.max(logReg.forward.pred[h,1:6,dim(logReg.forward.pred)[3]])
   }
+  logReg.acpF.perf <- table(y.app[folds==i],logReg.acpF.res)
+  logReg.acpF.error <-logReg.acpF.error + 1 - sum(diag(logReg.acpF.perf))/numberTest
+  
   logReg.acp1.perf <- table(y.app[folds==i],logReg.acp1.res)
   logReg.acp1.error <-logReg.acp1.error + 1 - sum(diag(logReg.acp1.perf))/numberTest
   
@@ -146,11 +156,13 @@ for(i in 1:K){
 }
 
 # Diviser le taux d'erreur par le nombre de K 
+logReg.acpF.error <-(logReg.acpF.error/K)*100
 logReg.acp1.error <-(logReg.acp1.error/K)*100
 logReg.acp2.error <-(logReg.acp2.error/K)*100
 logReg.lda.error <-(logReg.lda.error/K)*100
 logReg.forward.error <-(logReg.forward.error/K)*100
 
+print(logReg.acpF.error)
 print(logReg.acp1.error)
 print(logReg.acp2.error)
 print(logReg.lda.error)
