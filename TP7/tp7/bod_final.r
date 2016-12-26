abab1ae179bbe8a9313b8616595a4550e3bb726e
Valentin Paul <valentin.paul@etu.utc.fr>
2016-12-26 12:13:44 +0100

    forward added to all models

--- TP7/tp7/bod_final.r
+++ TP7/tp7/bod_final.r
@@ -61,27 +61,27 @@ folds <- sample(1:K,X.app.dim[1] ,replace=TRUE)
 
 tree.acp.error <- rep(0,159)
 tree.lda.error <- 0
-tree.forward.error <- rep(0, X.app.dim[2])
+tree.forward.error <- rep(0, 159)
 
 svm.acp.error <- rep(0,159)
 svm.lda.error <- 0
-svm.forward.error <- rep(0, X.app.dim[2])
+svm.forward.error <- rep(0, 159)
 
 svm.tune.acp.error <- rep(0,159)
 svm.tune.lda.error <- 0
-svm.tune.forward.error <- rep(0, X.app.dim[2])
+svm.tune.forward.error <- rep(0, 159)
 
 nb.acp.error <- rep(0,159)
 nb.lda.error <- 0
-nb.forward.error <- rep(0, X.app.dim[2])
+nb.forward.error <- rep(0,159)
 
 logReg.acp.error <- rep(0,159)
 logReg.lda.error <- 0
-logReg.forward.error <- rep(0, X.app.dim[2])
+logReg.forward.error <- rep(0, 159)
 
 lda.acp.error <- rep(0,159)
 lda.lda.error <- 0
-lda.forward.error <- rep(0, X.app.dim[2])
+lda.forward.error <- rep(0, 159)
 
 qda.acp.error <- rep(0,5)
 qda.lda.error <- 0
@@ -89,14 +89,18 @@ qda.forward.error <- rep(0,5)
 
 knn.acp.error <- matrix(0,nrow=numberKnn, ncol=159)
 knn.lda.error <- rep(0,numberKnn)
-knn.forward.error <- matrix(0,nrow=numberKnn, ncol=X.app.dim[2])
+knn.forward.error <- matrix(0,nrow=numberKnn, ncol=159)
 
 rf.acp.error <- matrix(0,nrow=length(vectorTree), ncol=159)
 rf.lda.error <- rep(0,length(vectorTree))
-rf.forward.error <- matrix(0,nrow=length(vectorTree), ncol=X.app.dim[2])
+rf.forward.error <- matrix(0,nrow=length(vectorTree), ncol=159)
+
+depart <- Sys.time()
 
 for(i in 1:K){
+  print("===")
   print(i)
+  print("===")
   # ====
   # Reduction de la dimensionnalite des variables
   # ===
@@ -106,17 +110,23 @@ for(i in 1:K){
   
   # 2) ACP
   X.acp <- prcomp(X.app)
-  X.acp.data <- X.acp$x
+  X.acp.data <- X.acp$x[,1:160] 
   
   # 3) Forward/Backward Selection
-  reg.fit<-regsubsets(y.app~.,data=as.data.frame(X.app),method="forward")
+  reg.fit<-regsubsets(y.app~.,data=as.data.frame(X.acp.data),method="forward", intercept=FALSE)
+  plot(reg.fit)
+  X.forward.data <- X.acp.data[,which(reg.fit$vorder == 1)]
+  X.forward.data <- as.data.frame(X.forward.data)
+  names(X.forward.data)[1] <- colnames(X.acp.data)[which(reg.fit$vorder == 1)]
 
   numberTest <- dim(X.lda.data[folds==i,])[1]
   
   # ===
   # ACP
   # ===
-  for(j in 2:160){
+  print("ACP")
+  #for(j in 2:160){
+    #print(j)
     # LDA
     #lda.acp <- lda(y.app[folds!=i]~., data=as.data.frame(X.acp.data[folds!=i,1:j]))
     #lda.acp.pred <- predict(lda.acp,newdata=as.data.frame(X.acp.data[folds==i,1:j]))
@@ -137,7 +147,7 @@ for(i in 1:K){
     #logReg.acp.pred <- predict(logReg.acp,newx=X.acp.data[folds==i,1:j],type="response",s=logReg.acp$lambda.min)
     
     #for (h in 1:numberTest) {
-     # logReg.acp.res[h] <-which.max(logReg.acp.pred[h,1:6,dim(logReg.acp.pred)[3]])
+     # logReg.acp.res[h] <-which.max(logReg.acp.pred[h,1:6,dim(logReg.acp.pred)[3]-1])
     #}
     
     #logReg.acp.perf <- table(y.app[folds==i],logReg.acp.res)
@@ -173,36 +183,82 @@ for(i in 1:K){
     #tree.acp.perf <- table(y.app[folds==i],tree.acp.pred)
     #tree.acp.error[j-1] <-tree.acp.error[j-1] + 1 - sum(diag(tree.acp.perf))/numberTest
     
-  }
+  #}
   
   # ===
   # Forward
   # ===
-  #for(j in 2:X.app.dim[2]){
+  print("Forward")
+  for(j in 2:160){
+    print(j)
+    X.forward.data[[colnames(X.acp.data)[which(reg.fit$vorder == j)]]] <- X.acp.data[,which(reg.fit$vorder == j)]
+
     # LDA
-   # print(j)
-    #lda.forward <- lda(y.app[folds!=i]~., data=as.data.frame(X.app[folds!=i,c(reg.fit$vorder[1:j])]))
-    #lda.forward.pred <- predict(lda.forward,newdata=as.data.frame(X.app[folds==i,c(reg.fit$vorder[1:j])]))
+    #lda.forward <- lda(y.app[folds!=i]~., data=as.data.frame(X.forward.data[folds!=i,1:j]))
+    #lda.forward.pred <- predict(lda.forward,newdata=as.data.frame(X.forward.data[folds==i,1:j]))
     #lda.forward.perf <- table(y.app[folds==i],lda.forward.pred$class)
     #lda.forward.error[j-1] <-lda.forward.error[j-1] + 1 - sum(diag(lda.forward.perf))/numberTest
     
     # KNN
     #for(number in 1:numberKnn){
-      # Forward
-     # knn.forward <- knn(as.data.frame(X.app[folds!=i,c(reg.fit$vorder[1:j])]), as.data.frame(X.app[folds==i,c(reg.fit$vorder[1:j])]), y.app[folds!=i],k=number)
+    # ACP Full
+     # knn.forward <- knn(as.data.frame(X.forward.data[folds!=i,1:j]), as.data.frame(X.forward.data[folds==i,1:j]), y.app[folds!=i],k=number)
     #  knn.forward.perf <- table(y.app[folds==i], knn.forward)
      # knn.forward.error[number,j-1] <-knn.forward.error[number,j-1]  + 1 - sum(diag(knn.forward.perf))/numberTest
     #}
-  #}
+    
+    # LogReg
+    #logReg.forward.res<-c(rep(0,numberTest))
+    #logReg.forward <- glmnet(as.matrix(X.forward.data[folds!=i,1:j]) ,y=y.app[folds!=i],family="multinomial")
+    #logReg.forward.pred <- predict(logReg.forward,newx=as.matrix(X.forward.data[folds==i,1:j]),type="response",s=logReg.forward$lambda.min)
+    #for (h in 1:numberTest) {
+     # logReg.forward.res[h] <-which.max(logReg.forward.pred[h,1:6,dim(logReg.forward.pred)[3] -1])
+    #}
+    
+    #logReg.forward.perf <- table(y.app[folds==i],logReg.forward.res)
+    #logReg.forward.error[j-1] <-logReg.forward.error[j-1] + 1 - sum(diag(logReg.forward.perf))/numberTest
+    
+    # Random Forest
+    #for(tree in vectorTree) {
+     # rf.forward <- randomForest(factor(y.app[folds!=i])~., data=as.data.frame(X.forward.data[folds!=i,1:j]), xtest=as.data.frame(X.forward.data[folds==i,1:j]), ytest=factor(y.app[folds==i]), ntree=tree)
+      #rf.forward.error[tree/100, j-1] <- rf.forward.error[tree/100, j-1] + 1 - (sum(diag(rf.forward$test$confusion)/numberTest))
+    #}
+    
+    # Naive Baeysien
+    #nb.forward <- naiveBayes(factor(y.app[folds!=i])~., data=as.data.frame(X.forward.data[folds!=i,1:j]))
+    #nb.forward.pred <- predict(nb.forward,newdata=as.data.frame(X.forward.data[folds==i,1:j]))
+    #nb.forward.perf <- table(factor(y.app[folds==i]),nb.forward.pred)
+    #nb.forward.error[j-1] <-nb.forward.error[j-1] + 1 - sum(diag(nb.forward.perf))/numberTest
+    
+    # SVM
+    #svm.forward <- svm(X.forward.data[folds!=i,1:j],y.app[folds!=i],type="C-classification")
+    #svm.forward.pred <- predict(svm.forward, X.forward.data[folds==i,1:j])
+    #svm.forward.perf <- table(factor(y.app[folds==i]),svm.forward.pred)
+    #svm.forward.error[j-1] <-svm.forward.error[j-1] + 1 - sum(diag(svm.forward.perf))/numberTest
+    
+    # SVM Tune 
+    #svm.tune.forward <- tune(svm, train.y = factor(y.app[folds!=i]),  train.x = X.forward.data[folds!=i,1:j], ranges = list(cost=10^(-1:2), gamma=c(.0005, .005, .05, .5, 1,2)))
+    #svm.tune.forward.pred <- predict(svm.tune.forward$best.model, X.forward.data[folds==i,1:j])
+    #svm.tune.forward.perf <- table(factor(y.app[folds==i]),svm.tune.forward.pred)
+    #svm.tune.forward.error[j-1] <-svm.tune.forward.error[j-1] + 1 - sum(diag(svm.tune.forward.perf))/numberTest
+    
+    # Tree
+    tree.forward <- tree(factor(y.app[folds!=i])~., data=as.data.frame(X.forward.data[folds!=i,1:j]))
+    tree.forward.pred <- predict(tree.forward,as.data.frame(X.forward.data[folds==i,1:j]), type="class")
+    tree.forward.perf <- table(y.app[folds==i],tree.forward.pred)
+    tree.forward.error[j-1] <-tree.forward.error[j-1] + 1 - sum(diag(tree.forward.perf))/numberTest
+  }
+  
   
   # ===
   # FDA
   # ===
+  print("FDA")
   # Random Forest
-  for(tree in vectorTree) {
-    rf.lda <- randomForest(factor(y.app[folds!=i])~., data=as.data.frame(X.lda.data[folds!=i,]), xtest=as.data.frame(X.lda.data[folds==i,]), ytest=factor(y.app[folds==i]), ntree=tree)
-    rf.lda.error[tree/100] <- rf.lda.error[tree/100] + 1 - (sum(diag(rf.lda$test$confusion)/numberTest))
-  }
+  #for(tree in vectorTree) {
+   # rf.lda <- randomForest(factor(y.app[folds!=i])~., data=as.data.frame(X.lda.data[folds!=i,]), xtest=as.data.frame(X.lda.data[folds==i,]), ytest=factor(y.app[folds==i]), ntree=tree)
+    #rf.lda.error[tree/100] <- rf.lda.error[tree/100] + 1 - (sum(diag(rf.lda$test$confusion)/numberTest))
+  #}
   
   # Tree
   #tree.lda <- tree(factor(y.app[folds!=i])~., data=as.data.frame(X.lda.data[folds!=i,]))
@@ -255,27 +311,26 @@ for(i in 1:K){
   # ===
   # Quadratic Discriminant Analysis
   # ===
+  print("QDA")
   for(j in 2:6){
     # ACP
-    qda.acp <- qda(y.app[folds!=i]~., data=as.data.frame(X.acp.data[folds!=i,1:j]))
-    qda.acp.pred <- predict(qda.acp,newdata=as.data.frame(X.acp.data[folds==i,1:j]))
-    qda.acp.perf <- table(y.app[folds==i],qda.acp.pred$class)
-    qda.acp.error[j-1] <-qda.acp.error[j-1] + 1 - sum(diag(qda.acp.perf))/numberTest
+   # qda.acp <- qda(y.app[folds!=i]~., data=as.data.frame(X.acp.data[folds!=i,1:j]))
+    #qda.acp.pred <- predict(qda.acp,newdata=as.data.frame(X.acp.data[folds==i,1:j]))
+    #qda.acp.perf <- table(y.app[folds==i],qda.acp.pred$class)
+    #qda.acp.error[j-1] <-qda.acp.error[j-1] + 1 - sum(diag(qda.acp.perf))/numberTest
     
     # Forward
-    qda.forward <- qda(y.app[folds!=i]~., data=as.data.frame(X.app[folds!=i,c(reg.fit$vorder[1:j])]))
-    qda.forward.pred <- predict(qda.forward,newdata=as.data.frame(X.app[folds==i,c(reg.fit$vorder[1:j])]))
+    qda.forward <- qda(y.app[folds!=i]~., data=as.data.frame(X.forward.data[folds!=i,1:j]))
+    qda.forward.pred <- predict(qda.forward,newdata=as.data.frame(X.forward.data[folds==i,1:j]))
     qda.forward.perf <- table(y.app[folds==i],qda.forward.pred$class)
     qda.forward.error[j-1] <-qda.forward.error[j-1] + 1 - sum(diag(qda.forward.perf))/numberTest
   }
   
   # FDA
-  qda.lda <- qda(y.app[folds!=i]~., data=as.data.frame(X.lda.data[folds!=i,]))
-  qda.lda.pred <- predict(qda.lda,newdata=as.data.frame(X.lda.data[folds==i,]))
-  qda.lda.perf <- table(y.app[folds==i],qda.lda.pred$class)
-  qda.lda.error <-qda.lda.error + 1 - sum(diag(qda.lda.perf))/numberTest
-  
-  
+  #qda.lda <- qda(y.app[folds!=i]~., data=as.data.frame(X.lda.data[folds!=i,]))
+  #qda.lda.pred <- predict(qda.lda,newdata=as.data.frame(X.lda.data[folds==i,]))
+  #qda.lda.perf <- table(y.app[folds==i],qda.lda.pred$class)
+  #qda.lda.error <-qda.lda.error + 1 - sum(diag(qda.lda.perf))/numberTest
 }
 
 # Diviser le taux d'erreur par le nombre de K 
@@ -318,45 +373,48 @@ rf.forward.error <- (rf.forward.error/K)*100
 print("QDA :")
 print(min(qda.acp.error))
 print(min(qda.lda.error))
-#print(min(qda.forward.error))
+print(min(qda.forward.error))
 
 print("LDA :")
 print(min(lda.acp.error))
 print(min(lda.lda.error))
-#print(lda.forward.error)
+print(min(lda.forward.error))
 
 print("KNN :")
 print(min(knn.acp.error))
 print(min(knn.lda.error))
-#print(knn.forward.error)
+print(min(knn.forward.error))
 
 print("RegLog :")
 print(min(logReg.acp.error))
 print(min(logReg.lda.error))
-#print(min(logReg.forward.error))
+print(min(logReg.forward.error))
 
 print("Naive Bayesien :")
 print(min(nb.acp.error))
 print(min(nb.lda.error))
-#print(min(nb.forward.error))
+print(min(nb.forward.error))
 
 print("SVM :")
 print(min(svm.acp.error))
 print(min(svm.lda.error))
-#print(min(svm.forward.error))
+print(min(svm.forward.error))
 
 print("SVM Tune:")
 print(min(svm.tune.acp.error))
 print(min(svm.tune.lda.error))
-#print(min(svm.tune.forward.error))
+print(min(svm.tune.forward.error))
 
 print("Tree:")
 print(min(tree.acp.error))
 print(min(tree.lda.error))
-#print(min(tree.forward.error))
+print(min(tree.forward.error))
 
 print("Random Forest :")
 print(min(rf.acp.error))
 print(min(rf.lda.error))
-#print(min(rf.forward.error))
+print(min(rf.forward.error))
 
+final <- Sys.time()
+print("temp final d'execution :")
+print(final-depart)
