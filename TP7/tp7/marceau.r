425d94e93f5bed2eb63a1d0ab9586054dc2ce4b3
mleriche <marceau.leriche@gmail.com>
2016-12-27 12:32:54 +0100

    neural network

--- TP7/tp7/marceau.r
+++ TP7/tp7/marceau.r
@@ -6,6 +6,19 @@ library(MASS)
 library(leaps)
 library(class)
 library("neuralnet")
+library(nnet)
+
+getFormulas <- function(col, order, label) {
+  result <- vector(mode="character", length=length(order))
+  for(i in 1:length(order)){
+    if(i == 1){
+      result[i] <- paste(c(as.character(label)," ~ ",col[order[i]]),collapse = '')
+    } else {
+      result[i] <- paste(c(result[i-1], col[order[i]]), collapse = ' + ')
+    }
+  }
+  return(result)
+}
 
 X.dim <- dim(X)
 
@@ -104,31 +117,6 @@ X.forward.test <- X.test[,which(reg.fit$vorder < ind)]
 traininginput <-  as.data.frame(X.acp.transform1)
 ideal <- class.ind(y.app)
 
-# --------------------- this is the method for a regression problem -----------------------------------
-
-
-
-#Column bind the data into one variable
-#trainingdata <- cbind(traininginput,trainingoutput)
-#Train the neural network
-#Going to have 10 hidden layers
-#Threshold is a numeric value specifying the threshold for the partial
-#derivatives of the error function as stopping criteria.
-#net.sqrt <- neuralnet(trainingdata[,25]~trainingdata[,1]+trainingdata[,2]+trainingdata[,3]+trainingdata[,4]+trainingdata[,5]+trainingdata[,6]+trainingdata[,7]+trainingdata[,8]+trainingdata[,9]+trainingdata[,10]+trainingdata[,11]+trainingdata[,12]+trainingdata[,13]+trainingdata[,14]+trainingdata[,15]+trainingdata[,16]+trainingdata[,17]+trainingdata[,18]+trainingdata[,19]+trainingdata[,20]+trainingdata[,21]+trainingdata[,22]+trainingdata[,23]+trainingdata[,24],trainingdata, hidden=10, threshold=0.01)
-#print(net.sqrt)
- 
-#Plot the neural network
-#plot(net.sqrt)
- 
-#Test the neural network on some training data
-#net.results <- compute(net.sqrt, X.acp.test.transform1) #Run them through the neural network
- 
-#Lets see what properties net.sqrt has
-#ls(net.results)
- 
-#Lets see the results
-#print(net.results$net.result)
- 
 
  # --------------------- this is the method for a classification problem -----------------------------------
 
@@ -177,4 +165,194 @@ forward.predict.net <- predict(forward.fit.net, X.forward.test, type="class")
 forward.perf.net <- table(forward.predict.net,y.test)
 forward.error.net <- 1-sum(diag(forward.perf.net))/length(y.test)
 print(forward.error.net)
+
+
+
+
+# pour la fonciton neuralnet()
+# cette ffonction est seulement utile lorsuq'il y a deux classes.
+data.train = X.lda.transform
+data.test = X.lda.transform.test
+ordre = c(1:dim(data.train)[2])
+
+
+misClassificationError1 = rep(0,10)
+misClassificationError2 = rep(0,10)
+misClassificationError3 = rep(0,10)
+misClassificationError4 = rep(0,10)
+misClassificationError5 = rep(0,10)
+misClassificationError6 = rep(0,10)
+neuralnet.error = rep(0,10)
+
+
+for(j in 1:10){
+	for(k in 1:6){
+		response <- rep(0,length(y.app))
+		for(i in 1:length(y.app)){
+			if(y.app[i]==k){
+				response[i]=1
+			}
+		}
+		newDataSet <- data.frame(data.train,response)
+		formule = getFormulas(colnames(newDataSet), ordre,"response")
+		if(k==1)
+		{
+			print("1/6")
+			neuralnet1 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn1 <- ifelse(neuralnet1$net.result[[1]]>0.5,1,0)
+			misClassificationError1[j] <- mean(response!=nn1)
+			print(misClassificationError1[j])
+		}
+		else if( k==2)
+		{
+			print("2/6")
+			neuralnet2 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn2 <- ifelse(neuralnet2$net.result[[1]]>0.5,1,0)
+			misClassificationError2[j] <- mean(response!=nn2)
+			print(misClassificationError2[j])
+		}
+		else if(k==3){
+			print("3/6")
+			neuralnet3 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn3 <- ifelse(neuralnet3$net.result[[1]]>0.5,1,0)
+			misClassificationError3[j] <- mean(response!=nn3)
+			print(misClassificationError3[j])
+		}
+		else if(k==4){
+			print("4/6")
+			neuralnet4 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn4 <- ifelse(neuralnet4$net.result[[1]]>0.5,1,0)
+			misClassificationError4[j] <- mean(response!=nn4)
+			print(misClassificationError4[j])
+		}
+		else if(k==5){
+			print("5/6")
+			neuralnet5 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn5 <- ifelse(neuralnet5$net.result[[1]]>0.5,1,0)
+			misClassificationError5[j] <- mean(response!=nn5)
+			print(misClassificationError5[j])
+		}
+		else if(k==6){
+			print("6/6")
+			neuralnet6 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,err.fct="ce",linear.output=FALSE)
+			nn6 <- ifelse(neuralnet6$net.result[[1]]>0.5,1,0)
+			misClassificationError6[j] <- mean(response!=nn6)
+			print(misClassificationError6[j])
+		}
+		
+	}
+
+	# predictions 
+
+	c1<-compute(neuralnet1,as.matrix(data.test))
+	c2<-compute(neuralnet2,as.matrix(data.test))
+	c3<-compute(neuralnet3,as.matrix(data.test))
+	c4<-compute(neuralnet4,as.matrix(data.test))
+	c5<-compute(neuralnet5,as.matrix(data.test))
+	c6<-compute(neuralnet6,as.matrix(data.test))
+
+	neuralnet.result <- rep(0,dim(data.test)[1])
+
+	for(i in 1:dim(data.test)[1]){
+		index <- which.max(c(c1$net.result[i],c2$net.result[i],c3$net.result[i],c4$net.result[i],c5$net.result[i],c6$net.result[i]))
+		neuralnet.result[i] <- index
+	}
+	neuralnet.perf <- table(neuralnet.result,y.test)
+	neuralnet.error[j] <- 1-sum(diag(neuralnet.perf))/length(y.test)
+	print(neuralnet.error[j])
+}
+print(mean(neuralnet.error))
+
+
+#confidence interval 
+
+#ci <- confidence.interval(neuralnet1,alpha=0.05)
+
+
+
+# neuralnet <- neuralnet(formule[5],newDataSet,hidden=c(2,3),linear.output=FALSE)
+# pour un probleme de regression il faut le mettre le critere linear.output a TRUE
+# pour un probleme de clasification il faut mettre le critere linear.output a FALSE
+# il faut utiliser la crossvalidation pour trouver le best hiddent layer possible
+# comme on utilise des vagues aleatoires il se peut que de temps en temps on ne puisse pas converger alors que des fois si
+
+
+# essayons maintenant avec l'algorithme de backproagation
+print("let s go for backpropagation")
+
+
+
+for(j in 1:10){
+	for(k in 1:6){
+		response <- rep(0,length(y.app))
+		for(i in 1:length(y.app)){
+			if(y.app[i]==k){
+				response[i]=1
+			}
+		}
+		newDataSet <- data.frame(data.train,response)
+		formule = getFormulas(colnames(newDataSet), ordre,"response")
+		if(k==1)
+		{
+			print("1/6")
+			neuralnet1 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn1 <- ifelse(neuralnet1$net.result[[1]]>0.5,1,0)
+			misClassificationError1[j] <- mean(response!=nn1)
+		}
+		else if( k==2)
+		{
+			print("2/6")
+			neuralnet2 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn2 <- ifelse(neuralnet2$net.result[[1]]>0.5,1,0)
+			misClassificationError2[j] <- mean(response!=nn2)
+		}
+		else if(k==3){
+			print("3/6")
+			neuralnet3 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn3 <- ifelse(neuralnet3$net.result[[1]]>0.5,1,0)
+			misClassificationError3[j] <- mean(response!=nn3)
+		}
+		else if(k==4){
+			print("4/6")
+			neuralnet4 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn4 <- ifelse(neuralnet4$net.result[[1]]>0.5,1,0)
+			misClassificationError4[j] <- mean(response!=nn4)
+		}
+		else if(k==5){
+			print("5/6")
+			neuralnet5 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn5 <- ifelse(neuralnet5$net.result[[1]]>0.5,1,0)
+			misClassificationError5[j] <- mean(response!=nn5)
+		}
+		else if(k==6){
+			print("6/6")
+			neuralnet6 <- neuralnet(formule[dim(data.train)[2]],newDataSet,hidden=6,algorithm="backprop",learningrate=0.01,err.fct="ce",linear.output=FALSE)
+			nn6 <- ifelse(neuralnet6$net.result[[1]]>0.5,1,0)
+			misClassificationError6[j] <- mean(response!=nn6)
+		}
+		
+	}
+	# predictions 
+
+	c1<-compute(neuralnet1,as.matrix(data.test))
+	c2<-compute(neuralnet2,as.matrix(data.test))
+	c3<-compute(neuralnet3,as.matrix(data.test))
+	c4<-compute(neuralnet4,as.matrix(data.test))
+	c5<-compute(neuralnet5,as.matrix(data.test))
+	c6<-compute(neuralnet6,as.matrix(data.test))
+
+	neuralnet.result <- rep(0,dim(data.test)[1])
+
+	for(i in 1:dim(data.test)[1]){
+		index <- which.max(c(c1$net.result[i],c2$net.result[i],c3$net.result[i],c4$net.result[i],c5$net.result[i],c6$net.result[i]))
+		neuralnet.result[i] <- index
+	}
+	neuralnet.perf <- table(neuralnet.result,y.test)
+	neuralnet.error[j] <- 1-sum(diag(neuralnet.perf))/length(y.test)
+	print(neuralnet.error[j])
+}
+print(mean(neuralnet.error))
+
+
+
 
