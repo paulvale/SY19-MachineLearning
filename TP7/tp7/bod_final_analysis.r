7eb02ce96d76b3191764f26ba31a20fc5d23adc8
Valentin Paul <valentin.paul@etu.utc.fr>
2017-01-09 21:48:23 +0100

    graph done

--- TP7/tp7/bod_final_analysis.r
+++ TP7/tp7/bod_final_analysis.r
@@ -4,102 +4,137 @@ load("~/Documents/UTC/A16/SY19/TPs_Desktop/TP/TP7/tp7/end_final.rData")
 # KNN
 # Evolution en fonction du nombre de composantes prises en comptes
 knn.acp.error.mean <- colMeans(knn.acp.error)
-plot(knn.acp.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / KNN")
-
 knn.forward.error.mean <- colMeans(knn.forward.error)
-plot(knn.forward.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / KNN")
-
 knn.f.error.mean <- colMeans(knn.f.error)
-plot(knn.f.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / KNN")
+
+min <- min(knn.acp.error.mean, knn.forward.error.mean, knn.f.error.mean)
+max <- max(knn.acp.error.mean, knn.forward.error.mean, knn.f.error.mean)
+
+plot(knn.acp.error.mean, type="l", col="blue",xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(knn.forward.error.mean, type="l",col="red", xlab="", ylab="", main="",ylim=c(min,max))
+par(new=TRUE)
+plot(knn.f.error.mean, type="l", col="green",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # Evolution en fonction du nombre K de voisins pris en compte
 knn.acp.error.mean <- rowMeans(knn.acp.error)
-plot(knn.acp.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with ACP Data / KNN")
-
 knn.forward.error.mean <- rowMeans(knn.forward.error)
-plot(knn.forward.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with Forward ACP / KNN")
-
 knn.f.error.mean <- rowMeans(knn.f.error)
-plot(knn.f.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with Forward Data / KNN")
 
-plot(knn.lda.error, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with FDA Data / KNN")
+min <- min(knn.acp.error.mean, knn.forward.error.mean, knn.f.error.mean, knn.lda.error)
+max <- max(knn.acp.error.mean, knn.forward.error.mean, knn.f.error.mean, knn.lda.error)
 
+plot(knn.acp.error.mean, type="l",col="blue", xlab="number of neighbors", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(knn.forward.error.mean, type="l", col="red",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(knn.f.error.mean, type="l", col="green",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(knn.lda.error, type="l", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # RF
 # Evolution en fonction du nombre de composantes prises en comptes
 rf.acp.error.mean <- colMeans(rf.acp.error)
-plot(rf.acp.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / RF")
-
 rf.forward.error.mean <- colMeans(rf.forward.error)
-plot(rf.forward.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / RF")
-
 rf.f.error.mean <- colMeans(rf.f.error)
-plot(rf.f.error.mean, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / RF")
+
+min <- min(rf.acp.error.mean, rf.forward.error.mean, rf.f.error.mean)
+max <- max(rf.acp.error.mean, rf.forward.error.mean, rf.f.error.mean)
+
+plot(rf.acp.error.mean, type="l", col="blue",xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(rf.forward.error.mean, type="l", col="red",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(rf.f.error.mean, type="l",col="green", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # Evolution en fonction du nombre K d'arbres pris en compte
 rf.acp.error.mean <- rowMeans(rf.acp.error)
-plot(rf.acp.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with ACP Data / RF")
-
 rf.forward.error.mean <- rowMeans(rf.forward.error)
-plot(rf.forward.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with Forward ACP / RF")
-
 rf.f.error.mean <- rowMeans(rf.f.error)
-plot(rf.f.error.mean, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with Forward Data / RF")
 
-plot(rf.lda.error, type="l", xlab="number of neighbors", ylab="MSE", main="Evolution of error with FDA Data / RF")
+min <- min(rf.acp.error.mean, rf.forward.error.mean, rf.f.error.mean, rf.lda.error)
+max <- max(rf.acp.error.mean, rf.forward.error.mean, rf.f.error.mean, rf.lda.error)
+
+plot(vectorTree,rf.acp.error.mean, type="l", col="blue",xlab="number of trees", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(vectorTree,rf.forward.error.mean, type="l", col="red",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(vectorTree,rf.f.error.mean, type="l",col="green", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(vectorTree,rf.lda.error, type="l", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # LDA
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(lda.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / LDA")
-
-plot(lda.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / LDA")
-
-plot(lda.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / LDA")
+#plot(lda.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / LDA")
+#plot(lda.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / LDA")
+#plot(lda.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / LDA")
 
 # QDA
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(qda.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / QDA")
-
-plot(qda.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / QDA")
-
-plot(qda.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / QDA")
+#plot(qda.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / QDA")
+#plot(qda.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / QDA")
+#plot(qda.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / QDA")
 
 # LogReg
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(logReg.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / LogReg")
+min <- min(logReg.acp.error, logReg.forward.error, logReg.f.error)
+max <- max(logReg.acp.error, logReg.forward.error, logReg.f.error)
 
-plot(logReg.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / LogReg")
-
-plot(logReg.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / LogReg")
+plot(logReg.acp.error, type="l",col="blue", xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(logReg.forward.error, type="l",col="red", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(logReg.f.error, type="l",col="green", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # NB
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(nb.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / NB")
+min <- min(nb.acp.error, nb.forward.error, nb.f.error)
+max <- max(nb.acp.error, nb.forward.error, nb.f.error)
 
-plot(nb.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / NB")
-
-plot(nb.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / NB")
+plot(nb.acp.error, type="l", col="blue",xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(nb.forward.error, type="l",col="red", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(nb.f.error, type="l", col="green",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # SVM
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(svm.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / SVM")
+min <- min(svm.acp.error, svm.forward.error, svm.f.error)
+max <- max(svm.acp.error, svm.forward.error, svm.f.error)
 
-plot(svm.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / SVM")
-
-plot(svm.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / SVM")
+plot(svm.acp.error, type="l", col="blue", xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(svm.forward.error, type="l", col="red",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(svm.f.error, type="l", col="green",xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
 
 # SVM Tune
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(svm.tune.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data Tune / SVM")
+min <- min(svm.tune.acp.error, svm.tune.forward.error, svm.tune.f.error)
+max <- max(svm.tune.acp.error, svm.tune.forward.error, svm.tune.f.error)
 
-plot(svm.tune.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP Tune / SVM")
-
-plot(svm.tune.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data Tune / SVM ")
+plot(svm.tune.acp.error, type="l", col="blue", xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(svm.tune.forward.error, type="l",col="red", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(svm.tune.f.error, type="l", col="green", xlab="", ylab="", main=" ", ylim=c(min,max))
+par(new=FALSE)
 
 # Tree
 # Evolution en fonction du nombre de composantes prises en comptes
-plot(tree.acp.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with ACP Data / Tree")
+min <- min(tree.acp.error, tree.forward.error, tree.f.error)
+max <- max(tree.acp.error, tree.forward.error, tree.f.error)
 
-plot(tree.forward.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward ACP / Tree")
-
-plot(tree.f.error, type="l", xlab="number of principal components", ylab="MSE", main="Evolution of error with Forward Data / Tree")
+plot(tree.acp.error, type="l",col="blue", xlab="number of principal components", ylab="MSE", main="Evolution of error", ylim=c(min,max))
+par(new=TRUE)
+plot(tree.forward.error, type="l",col="red", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=TRUE)
+plot(tree.f.error, type="l", col="green", xlab="", ylab="", main="", ylim=c(min,max))
+par(new=FALSE)
